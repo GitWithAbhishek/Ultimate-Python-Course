df7183df35694553de95870f5cebed375080bb85
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-10 08:36:45 +0530

    Completed Day 3

---
+++ Day 3/Lec_5.py
@@ -0,0 +1,10 @@
+print("Hello I am Good\nWhat About You")
+#This is single line comment
+print("Hello Abhishek")
+''''
+This is multi line comnment
+As you can see i can write here also 
+'''
+print("Aman","Pandey","Age ",22,end="\n")
+print("Naman","Pandey","Age ",24,sep="~")
+
---
+++ Day 3/Lec_6.py
@@ -0,0 +1,19 @@
+a = 2
+b = "Aman"
+c = True
+d = None
+e = complex(8 , 2)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+
+list =[2,2.4,[-3,23],["Aman","Naman"]];
+print(list);
+
+tuple =(1,2,3,4);
+print(tuple);
+
+dict ={1:"Aman","Age":20};
+print(dict)
