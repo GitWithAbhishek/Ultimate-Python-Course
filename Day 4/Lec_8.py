d07696f48fc05fe3f8da389ee6d6442ba67e1c5d
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-11 15:14:34 +0530

    Completed Day 4

---
+++ Day 4/Lec_8.py
@@ -0,0 +1,10 @@
+print("Solution")
+print("Making A Calculartor")
+num1 = 10;
+num2 = 3;
+print("Addition Of num1 + num2 = ",num1+num2)
+print("Subtraction Of num1 - num2 = ",num1-num2)
+print("Multiplication Of num1 * num2 = ",num1*num2)
+print("Division Of num1 / num2 = ",num1/num2)
+print("Floor Division Of num1 // num2 = ",num1//num2)
+print("Modulus Of num1 % num2 = ",num1%num2)
