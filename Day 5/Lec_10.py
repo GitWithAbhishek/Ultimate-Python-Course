895e16dca305872e6527325924d421a0c42d9c7d
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-12 22:08:43 +0530

    Completed Day 5

---
+++ Day 5/Lec_10.py
@@ -0,0 +1,7 @@
+print("Taking Input Fromn The User")
+
+a = input("Enter Your Name ");
+print("My Nmae is ",a);
+
+num = int(input("Enter A Number "));
+print("You Have Enter a Number That is ",num)
---
+++ Day 5/Lec_9.py
@@ -0,0 +1,8 @@
+print("Implicit Type Casting")
+a = 10.4;
+b = 2;
+print(a+b);
+print("Explicit Type Casting");
+num_string = "15";
+num = 5;
+print(int(num_string) + num);
