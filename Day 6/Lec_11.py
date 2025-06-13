47ffaf83ae7af0aa0a2f1cafd114b48714db40a5
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-13 22:08:30 +0530

    Completed Day 6

---
+++ Day 6/Lec_11.py
@@ -0,0 +1,9 @@
+print("Hello Strings");
+name = "Abhishek"
+friend = 'Gaurav'
+details = """Hello Guys
+We Are Good
+What About You"""
+print(name)
+print(friend)
+print(details)
---
+++ Day 6/Lec_12.py
@@ -0,0 +1,10 @@
+print("String Slicing Operations")
+
+fruit = "Mango"
+len1 = len(fruit);
+print("The Length Of Word Mango Is",len1)
+print(fruit[0:5]) # includiing 0 not 5
+print(fruit[0:2])
+print(fruit[:5])
+print(fruit[1:])
+print(fruit[0:-3]) # it will print as 0 to len-3
