7a4dd06cc8a10d37ad387c534bed22006e202428
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-16 06:34:33 +0530

    Completed Day 9

---
+++ Day 9/Lec_17.py
@@ -0,0 +1,17 @@
+name = "Abhishek"
+for i in name:
+    print(i)
+
+list1 = ["red","yellow","green","white"];
+for color in list1:
+    print(color)
+
+for i in range(11):
+    print(i)
+
+for i in range(4,11):
+    print(i)
+    
+for i in range(5,51,5):
+    print(i)
+    
---
+++ Day 9/Lec_18.py
@@ -0,0 +1,6 @@
+i = int(input("Enter A Numer "))
+while(i<10):
+    print(i ,"Hello Guys")
+    i=i+1
+else:
+    print("Out Of Loop")
