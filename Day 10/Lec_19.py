703c6660a8e1c769a070eca2c64b9f8621219adc
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-17 09:17:52 +0530

    Completed Day 10

---
+++ Day 10/Lec_19.py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+    if(i==5):
+        continue;
+    print("Hello ",i)
+
+for i in range(1,11):
+    if(i==5):
+        break;
+    print("Hello ",i)
---
+++ Day 10/Lec_20.py
@@ -0,0 +1,15 @@
+def greateroftwo(a,b):
+    if a>b:
+        print("A Is Greater")
+    else:
+        print("B Is Greater")
+
+def areaoftriangle(a,b):
+    area=(a*b)/2
+    print("Area Of Triangle Is ",area)
+
+def areaofrectangle(a,b):
+    pass                   # It is used to handle error without writting the function body code        
+
+greateroftwo(5,6);
+areaoftriangle(5,6);
