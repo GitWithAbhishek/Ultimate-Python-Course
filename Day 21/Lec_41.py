1dd2c174697ad4dbaac97496b30e35d61a4a8483
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-28 13:47:54 +0530

    Completed Day 21

---
+++ Day 21/Lec_41.py
@@ -0,0 +1,3 @@
+a=300
+b=200
+print(a) if a>b else print (b) if (a>(b+20)) else print("No")
---
+++ Day 21/Lec_42.py
@@ -0,0 +1,6 @@
+marks = [52,55,56,51,35,47,64]
+for index,mark in enumerate(marks):
+    print(mark)
+    if (index ==3):
+        print("Good")
+        
