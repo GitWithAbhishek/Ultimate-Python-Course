113fe111e8b35e227b85a3d8abe7221d28aa0b5c
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-06-29 10:50:04 +0530

    Completed Day 22

---
+++ Day 22/Lec_43.py
@@ -0,0 +1,14 @@
+# Create a virtual Enviroment
+# python -m venv myenv
+
+# Activate the virtual enviroment
+# myenv\Scripts\activate.bat
+
+# Deactivate the virtual enviroment
+# deactivate
+
+# Output the list of install packages
+# pip freeze > requirements.txt
+
+# Install the packages listed in the requirements.txt file
+# pip install -r requirements.txt
---
+++ Day 22/Lec_44.py
@@ -0,0 +1,17 @@
+import math # this will import all function
+result = math.sqrt(81)
+print(result)
+
+from math import sqrt ,pi
+result = sqrt(81)
+print(result)
+print(pi)
+
+# from math import * # This Will Import everything from math which is not recommended
+
+import math as m # this will make you easy
+result = m.sqrt(81)
+print(result)
+
+# Dir function - it tell you all function present in math
+print(dir(math))
