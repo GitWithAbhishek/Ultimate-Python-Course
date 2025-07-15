9be30c00e9755c90f5ff6d6cd9bf78acb8be2a11
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-07-15 23:13:59 +0530

    Completed Day 38

---
+++ Day 38/Lec_75.py
@@ -0,0 +1,9 @@
+import os
+# os.rename("Day 38/Clutter_Folder/file.txt","Day 34/Clutter_Folder/MyFile.txt")
+files = os.listdir("Day 38/Clutter_Folder")
+i=1
+for file in files:
+    if file.endswith(".png"):
+        print(file)
+        os.rename(f"Day 38/Clutter_Folder/{file}",f"Day 38/Clutter_Folder/{i}.png")
+        i=i+1
